f01206841f8e89d02950b3bac54c01a3a17d7603
Jeremy Woertink <jeremywoertink@gmail.com>
2016-03-07 09:49:59 -0800

    this seems to not be loaded when I thought. Ruby 2.3 should be required to use safe navigation

--- lib/jko_api/engine.rb
+++ lib/jko_api/engine.rb
@@ -5,7 +5,7 @@ module JkoApi
     initializer "jko_api.configure_rails_initialization" do |app|
       app.middleware.use JkoApi::Middleware
 
-      if JkoApi.configuration.authenticate?
+      if JkoApi.configuration&.authenticate?
         app.middleware.use "JkoApi::Strategies::#{JkoApi.configuration.strategy.to_s.capitalize}".constantize
       end
     end

0f8a4eab3acf527b162c07e650660ebd25e66d84
Jeremy Woertink <jeremywoertink@gmail.com>
2016-05-03 16:36:49 -0700

    spelling and stuff

--- lib/jko_api/engine.rb
+++ lib/jko_api/engine.rb
@@ -3,12 +3,11 @@ require 'rails/railtie'
 module JkoApi
   class Engine < ::Rails::Engine
     initializer "jko_api.configure_rails_initialization" do |app|
-      app.middleware.use JkoApi::Middleware, only: proc { |env| 
-        puts "ENVVVVVV", env.inspect
-        env['Accept'] && env['Accept'].include?('application/vnd.api') }
+      app.middleware.use JkoApi::Middleware, only: proc { |env|
+        env['HTTP_ACCEPT'] && env['HTTP_ACCEPT'].include?('application/vnd.api') }
 
       if JkoApi.configuration&.authenticate?
-        app.middleware.use "JkoApi::Strategies::#{JkoApi.configuration.strategy.to_s.capitalize}".constantize, only: proc { |env| env['Accept'] && env['Accept'].include?('application/vnd.api')  }
+        app.middleware.use "JkoApi::Strategies::#{JkoApi.configuration.strategy.to_s.capitalize}".constantize, only: proc { |env| env['HTTP_ACCEPT'] && env['HTTP_ACCEPT'].include?('application/vnd.api')  }
       end
     end
   end

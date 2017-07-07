d7bbc2256a007069e43b1a51bdc898ec57470fc3
Jeremy Woertink <jeremywoertink@gmail.com>
2017-07-07 09:48:53 -0700

    trying to port over some stuff from rails 4.2 to help with building modules dynamically

--- lib/jko_api/class_descendants_builder.rb
+++ lib/jko_api/class_descendants_builder.rb
@@ -33,8 +33,8 @@ module JkoApi
 
     def namespace(level)
       deconstantized = swap_level(level).deconstantize
-      unless Module.const_defined?(deconstantized)
-        Module.const_set deconstantized, Module.new
+      unless qualified_const_defined?(deconstantized)
+        qualified_const_set(deconstantized, Module.new)
       end
       deconstantized.constantize
     end
@@ -45,6 +45,36 @@ module JkoApi
 
     def initial_level
       descendant.name[LEVEL_REGEX].to_i
+    end
+
+    # Pulled from Rails 4.2 source
+    def qualified_const_defined?(path)
+      raise NameError, "#{path.inspect} is not a valid constant name!" unless /^(::)?([A-Z]\w*)(::[A-Z]\w*)*$/ =~ path
+
+      names = path.to_s.split('::')
+      names.shift if names.first.empty?
+
+      # We can't use defined? because it will invoke const_missing for the parent
+      # of the name we are checking.
+      names.inject(Module) do |mod, name|
+        return false unless mod.const_defined?(name, false)
+        mod.const_get name
+      end
+      return true
+    end
+
+    def qualified_const_set(path, value)
+      raise NameError.new("wrong constant name #$&") if path =~ /\A::[^:]+/
+      const_name = path.demodulize
+      mod_name = path.deconstantize
+      mod = mod_name.empty? ? self : qualified_const_get(mod_name)
+      mod.const_set(const_name, value)
+    end
+
+    def qualified_const_get(path)
+      path.split('::').inject(Module) do |mod, name|
+        mod.const_get(name)
+      end
     end
   end
 end
